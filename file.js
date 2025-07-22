1829d2287d9a6870505975788570c9150c65ef2e
zecky1120 <takahide1120@gmail.com>
2025-07-22 21:19:25 +0900

    add: 文字変換する関数を追加

--- file.js
+++ file.js
@@ -7,4 +7,35 @@ export default class File {
   constructor(file) {
     this.file = file;
   }
+
+  async convertMojiInPath() {
+    const absolutePath = path.resolve(this.file);
+    const stats = fs.statSync(absolutePath);
+    if (stats.isFile()) {
+      try {
+        const targetFile = await fsPromises.readFile(absolutePath, "utf-8");
+        const convertedFile = moji(targetFile).convert("ZE", "HE").toString();
+        await fsPromises.writeFile(absolutePath, convertedFile);
+      } catch (error) {
+        console.error(error.message);
+      }
+      this.#convertMojiInFile(this.file);
+    } else if (stats.isDirectory()) {
+      const dir = fs.readdirSync(absolutePath);
+      for (const item of dir) {
+        const itemPath = path.join(absolutePath, item);
+        await this.#convertMojiInFile(itemPath);
+      }
+    }
+  }
+
+  async #convertMojiInFile(file) {
+    try {
+      const targetFile = await fsPromises.readFile(file, "utf-8");
+      const convertedFile = moji(targetFile).convert("ZE", "HE").toString();
+      await fsPromises.writeFile(file, convertedFile);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 }

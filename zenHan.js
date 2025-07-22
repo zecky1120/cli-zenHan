95263c20908bd485418d88df03c4a39db78c9104
zecky1120 <takahide1120@gmail.com>
2025-07-22 21:32:50 +0900

    add: loading関数追加

--- zenHan.js
+++ zenHan.js
@@ -3,3 +3,17 @@
 import minimist from "minimist";
 import ora from "ora";
 import File from "./file.js";
+
+const argvs = minimist(process.argv.slice(2));
+
+class ZenHan {
+  constructor(argvs) {
+    this.argvs = argvs;
+  }
+
+  #spinnerAnimation() {
+    const spinner = ora("解析中...").start();
+    spinner.color = "yellow";
+    return spinner;
+  }
+}

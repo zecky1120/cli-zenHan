2b65dbb9c1925dc55ea4b39701d58e88933ddc63
zecky1120 <takahide1120@gmail.com>
2025-07-22 21:18:57 +0900

    add: import文追加

--- file.js
+++ file.js
@@ -1,3 +1,8 @@
+import fsPromises from "fs/promises";
+import fs from "fs";
+import path from "path";
+import moji from "moji";
+
 export default class File {
   constructor(file) {
     this.file = file;

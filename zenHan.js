66676d7a08c3326f0375c37f795d2676610d8d88
zecky1120 <takahide1120@gmail.com>
2025-07-22 21:33:48 +0900

    add: 実行関数追加

--- zenHan.js
+++ zenHan.js
@@ -11,9 +11,33 @@ class ZenHan {
     this.argvs = argvs;
   }
 
+  async run() {
+    const loading = this.#spinnerAnimation();
+    if (!this.argvs._.length) {
+      loading.fail("対象となるファイルかフォルダを指定してください。");
+      return;
+    }
+    try {
+      for (const argv of this.argvs._) {
+        await new File(argv).convertMojiInPath();
+      }
+      loading.succeed("解析が成功しました。");
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        console.error("\n対象のファイル及びディレクトリが存在しませんでした。");
+      } else {
+        console.error("\n" + error.message);
+      }
+      loading.fail("解析中にエラーが発生しました。");
+    }
+  }
+
   #spinnerAnimation() {
     const spinner = ora("解析中...").start();
     spinner.color = "yellow";
     return spinner;
   }
 }
+
+const zenHan = new ZenHan(argvs);
+zenHan.run();
